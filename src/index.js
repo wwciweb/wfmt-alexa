769c020e49d1d1224207717ed78d794207bc435f
Grant Smith <gsmith@wttw.com>
2018-06-05 18:04:03 -0500

    adding whats_on intent

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var express = require("express"),
     alexa = require("alexa-app"),
     request = require("request"),
+    fetch = require('node-fetch'),
     PORT = process.env.PORT || 3000,
     app = express(),
     // Setup the alexa app and attach it to express before anything else.
@@ -26,9 +27,9 @@ app.set("view engine", "ejs");
 
 function startStream(response) {
     response.audioPlayerPlayStream('REPLACE_ALL', {
-    token: config.stream.url,
-    url: config.stream.url,
-    offsetInMilliseconds: 0
+      token: config.stream.url,
+      url: config.stream.url,
+      offsetInMilliseconds: 0
   });
 }
 
@@ -57,6 +58,36 @@ alexaApp.intent("stream", {
       console.log("In play intent");
       startStream(response);
     }
+);
+
+alexaApp.intent("whats_on", {
+    "slots": {},
+    "utterances": [
+      "what is this track",
+      "what is this song",
+      "which track is this",
+      "what track is this",
+      "which song is this",
+      "what song is this",
+      "what's playing",
+      "what's currently playing",
+      "current song",
+      "current track",
+      "currently playing",
+      "what's on"
+    ]
+  }, function(req, response) {
+    console.log("In what's on intent");
+
+    return fetch("https://clients.webplaylist.org/cgi-bin/wfmt/wonV2.json").then((json) => {
+      return json.json()
+    }).then((json) => {
+      var currentlyPlaying = "On WFMT right now, " + json.track.title + " by " + json.track.composer;
+      response.say(currentlyPlaying);
+    }).catch((ex) => {
+      response.say("Sorry, something went wrong! Try again later.");
+    });
+  }
 );
 
 alexaApp.intent("AMAZON.CancelIntent", {
